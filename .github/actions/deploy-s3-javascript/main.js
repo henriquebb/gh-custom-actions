b4b9482e86f65408d94b4ae388bbe5668987e9c3
Henrique Barbosa <henrique.b.barbosa@bradesco.com.br>
2025-08-07 15:48:07 -0300

    teste

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,15 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const github = require('@actions/github'); //just to learn
 const exec = require('@actions/exec');
 
 function run() {
+    const bucket = core.getInput('bucket', { required: true });
+    const region = core.getInput('region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
     core.notice('Starting deployment to AWS S3');
 }
 
