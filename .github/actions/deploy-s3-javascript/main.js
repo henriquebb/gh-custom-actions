f50da535e95ae6adbb2cbff24a87233a3ffc8f5b
Henrique Barbosa <henrique.b.barbosa@bradesco.com.br>
2025-08-07 15:53:39 -0300

    teste

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,6 +11,9 @@ function run() {
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
     core.notice('Starting deployment to AWS S3');
+
+    const websiteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
